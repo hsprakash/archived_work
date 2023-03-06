f84da9cc9c1f0071d5ad6a463e1eef57ed551a82
hsprakash <summithari@gmail.com>
2023-03-06 14:21:16 -0800

    first commit

---
+++ apcsp/main.py
@@ -0,0 +1,16 @@
+# Author: Hari Prakash
+# Info: Main file for my apcsp performance task project
+####################################################################
+import sys
+import tkinter as tk 
+from tkinter import *
+
+sys.dont_write_bytecode = True
+
+
+window=tk.Tk()
+window.title("APCSP Performance Task")
+window.geometry("1080x720")
+
+
+window.mainloop()
